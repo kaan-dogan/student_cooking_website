6d6b19466421fa2c4d4b8cacd19bb972b128cd5d
kaan-dogan <kaandn51@gmail.com>
2023-11-15 23:33:27 +0000

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,210 @@
+/*Pizza Basics*/
+function displayPizza1(thisGraphic) {
+        document.getElementById('pizza1').style.display="block"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza2(thisGraphic) {
+        document.getElementById('pizza2').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza3(thisGraphic) {
+        document.getElementById('pizza3').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza4(thisGraphic) {
+        document.getElementById('pizza4').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza5(thisGraphic) {
+        document.getElementById('pizza5').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza6(thisGraphic) {
+        document.getElementById('pizza6').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza7(thisGraphic) {
+        document.getElementById('pizza7').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        document.getElementById('pizza8').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+    function displayPizza8(thisGraphic) {
+        document.getElementById('pizza8').style.display="block"
+        document.getElementById('pizza1').style.display="none"
+        document.getElementById('pizza3').style.display="none"
+        document.getElementById('pizza4').style.display="none"
+        document.getElementById('pizza5').style.display="none"
+        document.getElementById('pizza6').style.display="none"
+        document.getElementById('pizza7').style.display="none"
+        document.getElementById('pizza2').style.display="none"
+        thisGraphic.style.color="blue"
+    }
+/*Indian Spices*/
+function displayChili() {
+    document.getElementById("chili").style.display="block";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayCumin() {
+    document.getElementById("cumin").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayCinnamon() {
+    document.getElementById("cinnamon").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayCloves() {
+    document.getElementById("cloves").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayFenugreek() {
+    document.getElementById("fenugreek").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayCardamom() {
+    document.getElementById("cardamom").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayMustard() {
+    document.getElementById("mustard").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+    document.getElementById("nutmeg").style.display="none";
+}
+function displayNutmeg() {
+    document.getElementById("nutmeg").style.display="block";
+    document.getElementById("chili").style.display="none";
+    document.getElementById("cumin").style.display="none";
+    document.getElementById("cloves").style.display="none";
+    document.getElementById("fenugreek").style.display="none";
+    document.getElementById("cardamom").style.display="none";
+    document.getElementById("mustard").style.display="none";
+    document.getElementById("cinnamon").style.display="none";
+}
+/*Italian*/
+function getBigger(thisGraphic) { 
+    thisGraphic.style.width='170px';
+    thisGraphic.style.height='170px';
+    thisGraphic.style.margin='20px 10px 20px 0px'
+}
+function getNormal(thisGraphic) {
+    thisGraphic.style.width='140px'
+    thisGraphic.style.height='140px'
+    thisGraphic.style.margin='50px 40px 20px 0px'
+}
+/*Thai*/
+function displayThai1() {
+    document.getElementById("thaiLarge").src="../images/thai1.jpg";
+}
+function displayThai2() {
+    document.getElementById("thaiLarge").src="../images/thai2.jpg";
+}
+function displayThai3() {
+    document.getElementById("thaiLarge").src="../images/thai3.jpg";
+}
+function displayThai4() {
+    document.getElementById("thaiLarge").src="../images/thai4.jpg";
+}
+/*Indian Curry*/
+function displayChickenCurry() {
+    document.getElementById("chickenCurry").style.display="block";
+    document.getElementById("vegetableCurry").style.display="none";
+    document.getElementById("lambCurry").style.display="none";
+}
+function displayVegetableCurry() {
+    document.getElementById("chickenCurry").style.display="none";
+    document.getElementById("vegetableCurry").style.display="block";
+    document.getElementById("lambCurry").style.display="none";
+}
+function displayLambCurry() {
+    document.getElementById("chickenCurry").style.display="none";
+    document.getElementById("vegetableCurry").style.display="none";
+    document.getElementById("lambCurry").style.display="block";
+}
